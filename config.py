ac0827a070000c21b97b6131f2af45773e4cbc57
Jannikwendt <jwendt.ieu2021@student.ie.edu>
2025-05-31 19:41:20 +0200

    chore: add ProductionConfig and fix indentation

--- config.py
+++ config.py
@@ -3,29 +3,34 @@ import os
 
 load_dotenv()
 
+
 class Config(object):
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class LocalConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    # dbuser=os.getenv('DBUSER'),
-    # dbpass=os.getenv('DBPASS'),
-    # dbhost=os.getenv('DBHOST'),
-    # dbname=os.getenv('DBNAME')
-    # )
     # DEBUG = True
+
 
 class GithubCIConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     DEBUG = True
 
+
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST'),
-    dbname=os.getenv('DBNAME')
+    SQLALCHEMY_DATABASE_URI = (
+        'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+            dbuser=os.getenv('DBUSER'),
+            dbpass=os.getenv('DBPASS'),
+            dbhost=os.getenv('DBHOST'),
+            dbname=os.getenv('DBNAME'),
+        )
     )
     DEBUG = True
+
+
+class ProductionConfig(DevelopmentConfig):
+    """Minimal production settings so Flask can import this class without error."""
+    DEBUG = False
